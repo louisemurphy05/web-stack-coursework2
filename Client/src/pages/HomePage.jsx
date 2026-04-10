d0850757891f2e46a0b4fd4cf3425a1ff1bf15a5
Louise Murphy <2577435@dundee.ac.uk>
2026-04-11 00:41:25 +0100

    very basic starter code for each client page, added index.css for future styling too. - made route paths for each page (got rid of vite starter code it gave)

---
+++ Client/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>*TITLE*</h1>
+      <p></p>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ Client/src/pages/MoviePage.jsx
@@ -0,0 +1,11 @@
+function MoviePage() {
+  return (
+    <div>
+      <h1>Movie Page</h1>
+      <p>Movie details will show here</p>
+      <p>Reviews will go here too</p>
+    </div>
+  );
+}
+
+export default MoviePage;
